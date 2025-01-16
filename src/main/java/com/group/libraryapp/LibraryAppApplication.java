f3db72010e54ff0c34b9d6581d35162b398d32d2
minabee <mina223770@gmail.com>
2025-01-16 22:51:50 +0900

    연습

--- src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryAppApplication {
-
+  /*
+  main 함수 입니다.
+   */
   public static void main(String[] args) {
     SpringApplication.run(LibraryAppApplication.class, args);
   }
